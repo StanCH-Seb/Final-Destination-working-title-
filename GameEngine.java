0870225b9fc73fdb16f10272c83b2b9b6b4302b1
StanCH-Seb <anotherformypersonaluse@gmail.com>
2026-05-06 17:43:20 +0700

    Update GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -5,18 +5,19 @@ public class GameEngine {
     Scanner sc = new Scanner(System.in);
  
     Scene currentScene;
-    Scene scene1, scene2, scene3, scene4, scene5;
+    Scene scene1, scene2, scene3, scene4, scene5, scene6;
  
     public void startGame() {
  
         createScenes();
  
         while (true) {
- 
+    
+            System.out.println("Welcome to Final Destination!! (Working title)");
+            System.out.println("Main Menu");
             System.out.println("A. Start");
             System.out.println("B. Quit");
-            System.out.print("Choice: ");
- 
+            System.out.print("Choice: "); 
             String mainChoice = sc.nextLine().toUpperCase();
  
             if (mainChoice.equals("A")) {
@@ -29,10 +30,11 @@ public class GameEngine {
                     currentScene.displayScene();
                     System.out.print("Choice: ");
  
-                    String input = sc.nextLine().toUpperCase();
-                    int index = input.charAt(0) - 'A';
- 
-                    if (index >= 0 && index < currentScene.choices.size()) {
+                    String input = sc.nextLine().toUpperCase(); 
+                    int index = input.charAt(0) - 'A'; 
+
+               
+                    if (index >= 0 && index < currentScene.choices.size()) { 
                         gameActive = processChoice(currentScene.choices.get(index));
                     } else {
                         System.out.println("Invalid choice.");
@@ -55,12 +57,24 @@ public class GameEngine {
             System.out.println("Game Over");
             return false;
         }
+
+ 
+        if (currentScene.id == 6) {   
+            if (choice.description.equals("B")) {
+                System.out.println("\n The house explodes out of nowhere...");
+            }
+ 
+            System.out.println("Returning to main menu...");
+            return false;
+        }
  
         if (choice.nextScene == 1) currentScene = scene1;
         else if (choice.nextScene == 2) currentScene = scene2;
         else if (choice.nextScene == 3) currentScene = scene3;
         else if (choice.nextScene == 4) currentScene = scene4;
         else if (choice.nextScene == 5) currentScene = scene5;
+        else if (choice.nextScene == 6) currentScene = scene6;
+ 
  
         return true;
     }
@@ -77,24 +91,35 @@ public class GameEngine {
         scene2.addChoice(new Choice("Feed Biscuit", 0, true, "Dog got aggressive."));
         scene2.addChoice(new Choice("Pick Object", 3, false, null));
  
-        //  Scene 3 (4 choices allowed now)
+        //  Scene 3 
         scene3 = new Scene(3, "You are lost, phone dying...");
         scene3.addChoice(new Choice("Talk to man", 4, false, null));
         scene3.addChoice(new Choice("Enter alley", 0, true, "Robber kills you."));
         scene3.addChoice(new Choice("Wait", 0, true, "Dogs find you."));
         scene3.addChoice(new Choice("Sing song", 0, true, "Man stabs you."));
  
-        //  Scene 4 (4 choices)
+        //  Scene 4
         scene4 = new Scene(4, "The man misleads you...");
         scene4.addChoice(new Choice("Run", 0, true, "You fall."));
         scene4.addChoice(new Choice("Shout", 0, true, "Dogs hear you."));
         scene4.addChoice(new Choice("Climb gate", 0, true, "You fall."));
         scene4.addChoice(new Choice("Confront man", 5, false, null));
  
-        //  Scene 5 (final)
+        //  Scene 5 (pre-final)
         scene5 = new Scene(5, "You are close to home...");
         scene5.addChoice(new Choice("Run home", 0, true, "Hit by car."));
         scene5.addChoice(new Choice("Walk safely", 6, false, null));
+
+        // //Scene 6 (finale)
+        scene6 = new Scene(6, "...\nEverything goes silent.\nThe world fades to black.");
+        scene6.addChoice(new Choice("A", 0, false, null));
+        scene6.addChoice(new Choice("B", 0, false, "Explosion ending"));
+
+     //IMPORTANT NOTE: The scenes here are summarized!!
+     //I will be adding the long scenes in the future
+     //Thanks 
+     //-SEBLOS
+
     }
 }
  
