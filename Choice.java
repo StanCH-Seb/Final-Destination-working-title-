ba32084030b4f41eab95ed95e77ead4c4a0907c9
StanSeblos-Ch <stanchriven.seblos@cit.edu>
2026-05-09 15:15:25 +0700

    updated all Java files

--- Choice.java
+++ Choice.java
@@ -1,13 +1,27 @@
 public class Choice {
-    String description;
-    int nextScene;
-    boolean gameOver;
-    String deathMessage; 
+    private String description;
+    private int nextScene;
+    private boolean gameOver;
+    private String deathMessage;
     
     public Choice(String description, int nextScene, boolean gameOver, String deathMessage) {
         this.description = description;
         this.nextScene = nextScene;
         this.gameOver = gameOver;
         this.deathMessage = deathMessage;
+    }
+ 
+    //Getters for Choice
+    public String getDescription(){
+        return description;
+     }
+    public int getNextScene() {
+        return nextScene;
+    }
+    public boolean isGameOver() {
+        return gameOver;
+    }
+    public String getDeathMessage() {
+        return deathMessage;
     }
 }
--- GameEngine.java
+++ GameEngine.java
@@ -4,7 +4,8 @@ public class GameEngine {
  
     Scanner sc = new Scanner(System.in);
  
-    Scene currentScene;
+    GameObject currentScene;
+ 
     Scene scene1, scene2, scene3, scene4, scene5, scene6;
  
     public void startGame() {
@@ -12,12 +13,13 @@ public class GameEngine {
         createScenes();
  
         while (true) {
-    
+ 
             System.out.println("Welcome to Final Destination!! (Working title)");
             System.out.println("Main Menu");
             System.out.println("A. Start");
             System.out.println("B. Quit");
-            System.out.print("Choice: "); 
+            System.out.print("Choice: ");
+ 
             String mainChoice = sc.nextLine().toUpperCase();
  
             if (mainChoice.equals("A")) {
@@ -28,98 +30,257 @@ public class GameEngine {
                 while (gameActive) {
  
                     currentScene.displayScene();
+ 
                     System.out.print("Choice: ");
  
-                    String input = sc.nextLine().toUpperCase(); 
-                    int index = input.charAt(0) - 'A'; 
-
-               
-                    if (index >= 0 && index < currentScene.choices.size()) { 
-                        gameActive = processChoice(currentScene.choices.get(index));
+                    String input = sc.nextLine().toUpperCase();
+ 
+                    int index = input.charAt(0) - 'A';
+ 
+                    Scene tempScene = (Scene) currentScene;
+ 
+                    if (index >= 0 && index < tempScene.getChoices().size()) {
+ 
+                        gameActive = processChoice(
+                                tempScene.getChoices().get(index)
+                        );
+ 
                     } else {
+ 
                         System.out.println("Invalid choice.");
+ 
                     }
                 }
  
             } else if (mainChoice.equals("B")) {
+ 
                 System.out.println("Thanks for playing!");
                 System.exit(0);
+ 
             } else {
+ 
                 System.out.println("Invalid choice.");
+ 
             }
         }
     }
  
     public boolean processChoice(Choice choice) {
  
-        if (choice.gameOver) {
-            System.out.println("\n" + choice.deathMessage);
-            System.out.println("Game Over");
-            return false;
-        }
-
+        if (choice.isGameOver()) {
  
-        if (currentScene.id == 6) {   
-            if (choice.description.equals("B")) {
-                System.out.println("\n The house explodes out of nowhere...");
+            System.out.println("\n" + choice.getDeathMessage());
+            System.out.println("\nGame Over!\n");
+ 
+            return false;
+ 
+        }
+ 
+        if (((Scene) currentScene).getId() == 6) {
+ 
+            if (choice.getDescription().equals("B")) {
+ 
+                System.out.println("\nThe house explodes out of nowhere...");
+ 
             }
  
             System.out.println("Returning to main menu...");
-            return false;
-        }
-        //change to a switch statement if more scenes are added
-        if (choice.nextScene == 1) currentScene = scene1;
-        else if (choice.nextScene == 2) currentScene = scene2;
-        else if (choice.nextScene == 3) currentScene = scene3;
-        else if (choice.nextScene == 4) currentScene = scene4;
-        else if (choice.nextScene == 5) currentScene = scene5;
-        else if (choice.nextScene == 6) currentScene = scene6;
  
+            return false;
+ 
+        }
+ 
+        if (choice.getNextScene() == 1) currentScene = scene1;
+        else if (choice.getNextScene() == 2) currentScene = scene2;
+        else if (choice.getNextScene() == 3) currentScene = scene3;
+        else if (choice.getNextScene() == 4) currentScene = scene4;
+        else if (choice.getNextScene() == 5) currentScene = scene5;
+        else if (choice.getNextScene() == 6) currentScene = scene6;
  
         return true;
     }
  
     public void createScenes() {
+        // Scene 1
+        scene1 = new Scene(
+                1,
+                "You feel your usual path will kill you..."
+        );
  
-        //  Scene 1
-        scene1 = new Scene(1, "You feel your usual path will kill you...");
-        scene1.addChoice(new Choice("Same Path", 0, true, "Aircon falls on your head."));
-        scene1.addChoice(new Choice("Different Path", 2, false, null));
+        scene1.addChoice(
+                new Choice(
+                        "Same Path",
+                        0,
+                        true,
+                        "Aircon falls on your head."
+                )
+        );
  
-        //  Scene 2
-        scene2 = new Scene(2, "A dog blocks your way...");
-        scene2.addChoice(new Choice("Feed Biscuit", 0, true, "Dog got aggressive."));
-        scene2.addChoice(new Choice("Pick Object", 3, false, null));
+        scene1.addChoice(
+                new Choice(
+                        "Different Path",
+                        2,
+                        false,
+                        null
+                )
+        );
  
-        //  Scene 3 
-        scene3 = new Scene(3, "You are lost, phone dying...");
-        scene3.addChoice(new Choice("Talk to man", 4, false, null));
-        scene3.addChoice(new Choice("Enter alley", 0, true, "Robber kills you."));
-        scene3.addChoice(new Choice("Wait", 0, true, "Dogs find you."));
-        scene3.addChoice(new Choice("Sing song", 0, true, "Man stabs you."));
+      
+        // Scene 2
+        scene2 = new Scene(
+                2,
+                "A dog blocks your way..."
+        );
  
-        //  Scene 4
-        scene4 = new Scene(4, "The man misleads you...");
-        scene4.addChoice(new Choice("Run", 0, true, "You fall."));
-        scene4.addChoice(new Choice("Shout", 0, true, "Dogs hear you."));
-        scene4.addChoice(new Choice("Climb gate", 0, true, "You fall."));
-        scene4.addChoice(new Choice("Confront man", 5, false, null));
+        scene2.addChoice(
+                new Choice(
+                        "Feed Biscuit",
+                        0,
+                        true,
+                        "Dog got aggressive."
+                )
+        );
  
-        //  Scene 5 (pre-final)
-        scene5 = new Scene(5, "You are close to home...");
-        scene5.addChoice(new Choice("Run home", 0, true, "Hit by car."));
-        scene5.addChoice(new Choice("Walk safely", 6, false, null));
+        scene2.addChoice(
+                new Choice(
+                        "Pick Object",
+                        3,
+                        false,
+                        null
+                )
+        );
+ 
 
-        // //Scene 6 (finale)
-        scene6 = new Scene(6, "...\nEverything goes silent.\nThe world fades to black.");
-        scene6.addChoice(new Choice("A", 0, false, null));
-        scene6.addChoice(new Choice("B", 0, false, "Explosion ending"));
-
-       // NOTE:
-       // Scenes are currently summarized for testing purposes.
-       // Full narrative scenes will be added in future updates.
-       // - SEBLOS
-
+        // Scene 3
+        scene3 = new Scene(
+                3,
+                "You are lost, phone dying..."
+        );
+ 
+        scene3.addChoice(
+                new Choice(
+                        "Talk to man",
+                        4,
+                        false,
+                        null
+                )
+        );
+ 
+        scene3.addChoice(
+                new Choice(
+                        "Enter alley",
+                        0,
+                        true,
+                        "Robber kills you."
+                )
+        );
+ 
+        scene3.addChoice(
+                new Choice(
+                        "Wait",
+                        0,
+                        true,
+                        "Dogs find you."
+                )
+        );
+ 
+        scene3.addChoice(
+                new Choice(
+                        "Sing song",
+                        0,
+                        true,
+                        "Man stabs you."
+                )
+        );
+ 
+        // Scene 4
+        scene4 = new Scene(
+                4,
+                "The man misleads you..."
+        );
+ 
+        scene4.addChoice(
+                new Choice(
+                        "Run",
+                        0,
+                        true,
+                        "You fall."
+                )
+        );
+ 
+        scene4.addChoice(
+                new Choice(
+                        "Shout",
+                        0,
+                        true,
+                        "Dogs hear you."
+                )
+        );
+ 
+        scene4.addChoice(
+                new Choice(
+                        "Climb gate",
+                        0,
+                        true,
+                        "You fall."
+                )
+        );
+ 
+        scene4.addChoice(
+                new Choice(
+                        "Confront man",
+                        5,
+                        false,
+                        null
+                )
+        );
+ 
+        // Scene 5
+        scene5 = new Scene(
+                5,
+                "You are close to home..."
+        );
+ 
+        scene5.addChoice(
+                new Choice(
+                        "Run home",
+                        0,
+                        true,
+                        "Hit by car."
+                )
+        );
+ 
+        scene5.addChoice(
+                new Choice(
+                        "Walk safely",
+                        6,
+                        false,
+                        null
+                )
+        );
+ 
+        // Scene 6
+        scene6 = new Scene(
+                6,
+                "...\nEverything goes silent.\nThe world fades to black."
+        );
+ 
+        scene6.addChoice(
+                new Choice(
+                        "A",
+                        0,
+                        false,
+                        null
+                )
+        );
+ 
+        scene6.addChoice(
+                new Choice(
+                        "B",
+                        0,
+                        false,
+                        "Explosion ending"
+                )
+        );
     }
 }
- 
